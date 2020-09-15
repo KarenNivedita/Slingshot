372a3d18b86e3d5a01d9fc5cf4696891cd1c1eba
KarenNivedita <rohinifellowship1718@gmail.com>
2020-09-15 14:44:04 +0530

    slingshot was made

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.slingShot = Constraint.create(options);
+        World.add(world, this.slingShot);
+    }
+
+    display(){
+        if(this.slingShot.bodyA){
+        var pointA = this.slingShot.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+     free(){
+         this.slingShot.bodyA = null;
+     }
+}
